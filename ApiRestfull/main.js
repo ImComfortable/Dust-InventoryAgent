790c3f403480c94f586fa10e48d50537aa8b019e
Comfortable <88853070+ImComfortable@users.noreply.github.com>
2025-02-06 16:22:19 -0300

    Update main.js

--- ApiRestfull/main.js
+++ ApiRestfull/main.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 app.use(express.json());
 
-mongoose.connect('mongodb://agente:JolyneTheCat120207@localhost:27017/InfosPC')
+mongoose.connect('mongodb://mongoconnectURL')
    .then(() => console.log('Conectado ao Mongodb'))
    .catch((err) => console.error('Error ao conectar ao mongo', err));
 
@@ -68,5 +68,5 @@ mongoose.connect('mongodb://agente:JolyneTheCat120207@localhost:27017/InfosPC')
 
 // Inicia o servidor
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://192.168.20.8:${port}`);
+    console.log(`Servidor rodando em http://192.168.0.0:${port}`);
 });
